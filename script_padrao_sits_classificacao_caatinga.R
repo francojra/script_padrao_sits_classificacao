777fd411ffae3abadfde78b30e0e449ed96eddf3
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-08-17 21:58:09 -0300

    Add files via upload

---
+++ script_padrao_sits_classificacao_caatinga.R
@@ -0,0 +1,75 @@
+# Script para Classificação do Bioma Caatinga -------------------------------------------------------------------------------------------------------------
+# Classificação por Região de Mapeamento (RM) -------------------------------------------------------------------------------------------------------------
+# Região de mapeamento: xxx -----------------------------------------------------------------------------------------------------------------------------------
+# Tiles: xxx --------------------------------------------------------------------------------------------------------------------------------------------------
+# Bandas e índices: xxx ---------------------------------------------------------------------------------------------------------------------------------------
+
+# Carregar pacotes ----------------------------------------------------------------------------------------------------------------------------------------
+
+library(tibble) # Pacote para visualizar tabelas
+library(sits) # Pacote para análises de séries temporais de imagens de satélite
+library(sitsdata) # Pacote para obter conjunto de dados de amostras do sits
+library(kohonen) # Pacote para plotar o mapa SOM
+library(randomForestExplainer) # Pacote para treinar modelo de classificação
+library(luz) # Pacote para facilitar criação, treino e avaliação de modelos no Torch
+library(torch) # Pacote para criar modelos deep learning e treinar redes neurais
+torch::install_torch()
+library(tidyverse) # Pacote para manipulação de tabelas e gráficos
+library(terra) # Pacote para manipular dados espaciais (imagens raster, dados de satélite)
+library(raster) # Pacote mais antigo para manipulação de dados raster
+library(sf) # Pacote para manipulação de dados vetoriais (pontos, linhas, polígonos)
+
+# Criar e ler cubo de dados -------------------------------------------------------------------------------------------------------------------------------
+
+## Salvar cubo 
+
+# Ler arquivo .shp com amostras por classes ---------------------------------------------------------------------------------------------------------------
+
+
+# Adicionar amostras ao cubo de dados criado --------------------------------------------------------------------------------------------------------------
+
+## Salvar cubo com amostras
+
+# Visualizar padrões de séries temporais por classe -------------------------------------------------------------------------------------------------------
+
+## Gráfico
+
+# Balanceamento de amostras -------------------------------------------------------------------------------------------------------------------------------
+
+
+# Análise SOM ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Seleção de neurônios no SOM -----------------------------------------------------------------------------------------------------------------------------
+
+
+# Detectar ruídos das amostras ----------------------------------------------------------------------------------------------------------------------------
+
+
+# Remover amostras ruidosas -------------------------------------------------------------------------------------------------------------------------------
+
+
+# Ver diferenças na quantidade de amostras antes e após filtragem -----------------------------------------------------------------------------------------
+
+
+# Gerar SOM dos dados sem ruídos --------------------------------------------------------------------------------------------------------------------------
+
+
+# Avaliar matriz de confusão das amostras antes e após filtragem ------------------------------------------------------------------------------------------
+
+
+# Classificações ------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Treinar modelo Random Forest ----------------------------------------------------------------------------------------------------------------------------
+
+
+# Validação do modelo Random Forest -----------------------------------------------------------------------------------------------------------------------
+
+
+# Produzir mapa de probabilidades de classes --------------------------------------------------------------------------------------------------------------
+
+
+# Unir tiles com sits_mosaic() ----------------------------------------------------------------------------------------------------------------------------
+
+
